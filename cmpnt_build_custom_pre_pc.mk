8e8e04f26d2f8e8dd029285ecb563976a4e3a0ce
Steve Maynard <steve.maynard@twcable.com>
2014-08-06 14:33:46 -0600

    initial commit from CCSP source

---
+++ cmpnt_build_custom_pre_pc.mk
@@ -0,0 +1,38 @@
+#######################################################################
+#   Copyright [2014] [Cisco Systems, Inc.]
+# 
+#   Licensed under the Apache License, Version 2.0 (the \"License\");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+# 
+#       http://www.apache.org/licenses/LICENSE-2.0
+# 
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an \"AS IS\" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#######################################################################
+
+#
+#   for RDKB
+#   pre custom makefile, included by component makefile
+#
+
+#
+#   component list
+#
+CCSP_MISC_BUILD_dmcli           := 1
+CCSP_MISC_BUILD_psmcli          := 1
+
+CCSP_MISC_BUILD_RecoveryManager := 0
+CCSP_MISC_BUILD_cdm             := 0
+
+CCSP_MISC_BUILD_CoreDump        := 0
+
+CCSP_MISC_BUILD_TR_DBG_UI       := 0
+
+#
+# Custom build definitions
+#
+CFLAGS  += -DCCSP_INC_no_asm_sigcontext_h
