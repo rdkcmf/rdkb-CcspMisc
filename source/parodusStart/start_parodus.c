a9a6baf264dc6e07557986055b607213836b0b02
Gayathri <gayathri_buddappagari@cable.comcast.com>
2017-08-21 16:43:30 +0530

    RDKB-13642,RDKB-13473 : Sync during upgrade from webpa release to parodus
    Reason for change:
    RDKB-13642: To get/set values from/to DB
    RDKB-13473: As part of parodus CMC, CID, SyncProtocolVersion parameters were moved from syscfg.db to bbhm , so these values needs to synced during upgrade.
    Test Procedure:
    RDKB-13642: Verify build
    RDKB-13473: Upgrade from release image to parodus distro ON build. Check ArmConsolelog.txt.0 and confirm PSM db sync happened successfully.
    Risks: None
    Signed-off-by: sadhyama_vengilat2@cable.comcast.com​
    
    Change-Id: I9756e1046b9b3a78ce89335fa0a9fb8317d5324f

--- source/parodusStart/start_parodus.c
+++ source/parodusStart/start_parodus.c
@@ -47,6 +47,7 @@
 #include <ccsp/platform_hal.h>
 #include <ccsp/cm_hal.h>
 #include <autoconf.h>
+#include "cJSON.h"
 
 #ifdef _PLATFORM_RASPBERRYPI_
 #include "ccsp_vendor.h"
@@ -56,7 +57,13 @@
 #define DEVICE_PROPS_FILE             "/etc/device.properties"
 #define MODULE 			      "PARODUS"
 #define MAX_BUF_SIZE 		      1024
-#define LogInfo(...)                  _START_LOG(__VA_ARGS__)
+#define MAX_VALUE_SIZE 		      32
+#define LOG_ERROR                     0
+#define LOG_INFO                      1
+#define LogInfo(...)                  _START_LOG(LOG_INFO, __VA_ARGS__)
+#define LogError(...)                 _START_LOG(LOG_ERROR, __VA_ARGS__)
+#define WEBPA_CFG_FILE		      "/nvram/webpa_cfg.json"
+#define WEBPA_CFG_FIRMWARE_VER	      "oldFirmwareVersion"
 
 #ifdef CONFIG_CISCO
 #define CONFIG_VENDOR_NAME  "Cisco"
@@ -66,8 +73,15 @@
 /*----------------------------------------------------------------------------*/
 static void get_url(char *parodus_url, char *seshat_url);
 static int addParodusCmdToFile(char *command);
-static void _START_LOG(const char *msg, ...);
-
+static void _START_LOG(int level, const char *msg, ...);
+static void getFirmwareFromJson(cJSON **json, char * cfgJson_firmware);
+static int  writeToJson(char *data);
+static void getValuesFromPsmDb(char *names[], char **values,int count);
+static void getValuesFromSysCfgDb(char *names[], char **values,int count);
+static int setValuesToPsmDb(char *names[], char **values,int count);
+static int syncXpcParamsOnUpgrade(char *lastRebootReason, char *firmwareVersion);
+static void free_sync_db_items(int paramCount,char *psmValues[],char *sysCfgValues[],cJSON *json);
+static char *pathPrefix  = "eRT.com.cisco.spvtg.ccsp.webpa.";
 /*----------------------------------------------------------------------------*/
 /*                             External Functions                             */
 /*----------------------------------------------------------------------------*/
@@ -88,7 +102,7 @@ int main()
         struct sysinfo s_info;
         struct timeval currentTime;
        	int cmdUpdateStatus = -1;
-        int upTime=0;	
+        int upTime=0, syncStatus= -1;
 
         if ( platform_hal_PandMDBInit() == 0)
         {
@@ -96,7 +110,7 @@ int main()
         }
         else
         {
-                LogInfo("Failed to initiate DB\n");
+                LogError("Failed to initiate DB\n");
         }
         
         if ( cm_hal_InitDB() == 0)
@@ -105,7 +119,7 @@ int main()
         }
         else
         {
-                LogInfo("Failed to initiate cm_hal DB\n");
+                LogError("Failed to initiate cm_hal DB\n");
         }
 
 	if ( platform_hal_GetModelName(modelName) == 0)
@@ -114,7 +128,7 @@ int main()
 	}
         else 
         {
-        	LogInfo("Unable to get ModelName\n");
+        	LogError("Unable to get ModelName\n");
         	
     	}
 
@@ -124,7 +138,7 @@ int main()
 	}
         else 
         {
-        	LogInfo("Unable to get SerialNumber\n");
+        	LogError("Unable to get SerialNumber\n");
     	}
     	
     	if ( platform_hal_GetFirmwareName(firmwareVersion, 64) == 0)
@@ -133,7 +147,7 @@ int main()
 	}
         else 
         {
-        	LogInfo("Unable to get FirmwareName\n");
+        	LogError("Unable to get FirmwareName\n");
     	}
     	
     	if(strlen(CONFIG_VENDOR_NAME) > 0)
@@ -144,13 +158,13 @@ int main()
         }
         else
         {
-         	LogInfo("Unable to get Manufacturer Name\n");
+         	LogError("Unable to get Manufacturer Name\n");
         }
     	
     	
     	if (syscfg_init() != 0)
         {
-        	LogInfo("syscfg init failure\n");
+        	LogError("syscfg init failure\n");
         	strcpy(lastRebootReason, "unknown");
         }
         else
@@ -168,7 +182,7 @@ int main()
          }
          else
          {
-         	LogInfo("Unable to get MACAdress\n");
+         	LogError("Unable to get MACAdress\n");
          }
          
          
@@ -176,7 +190,7 @@ int main()
         {
                 if(sysinfo(&s_info))
                 {
-                        LogInfo("Failure in sysinfo fetch.\n");
+                        LogError("Failure in sysinfo fetch.\n");
                 }
 		else
 		{
@@ -192,7 +206,7 @@ int main()
         }
         else
         {
-                LogInfo("Unable to get bootTime\n");
+                LogError("Unable to get bootTime\n");
 	}
 
          LogInfo("Fetch parodus url from device.properties file\n");
@@ -220,9 +234,22 @@ int main()
 	}
 	else
 	{
-		LogInfo("Error in adding parodus cmd to file\n");
+		LogError("Error in adding parodus cmd to file\n");
 	}
 
+	syncStatus = syncXpcParamsOnUpgrade(lastRebootReason, firmwareVersion);
+	if(syncStatus == 0)
+	{
+		LogInfo("DB synced successfully on firmware upgrade\n");
+	}
+	else if(syncStatus == -2)
+	{
+		LogInfo("PARODUS: Failed to sync DB during Firmware Upgrade\n");
+	}
+	else
+	{
+		LogInfo("DB sync is not required or failed to sync!!\n");
+	}
 	return 0;
 }
 
@@ -258,19 +285,19 @@ static void get_url(char *parodus_url, char *seshat_url)
 	}
 	else
 	{
-		LogInfo("Failed to open device.properties file:%s\n", DEVICE_PROPS_FILE);
+		LogError("Failed to open device.properties file:%s\n", DEVICE_PROPS_FILE);
 	}
 	fclose(fp);
 	
 	if (0 == parodus_url[0])
 	{
-		LogInfo("parodus_url is not present in device. properties:%s\n", parodus_url);
+		LogError("parodus_url is not present in device. properties:%s\n", parodus_url);
 	
 	}
 	
         if (0 == seshat_url[0])
         {
-                LogInfo("seshat_url is not present in device. properties:%s\n", seshat_url);
+                LogError("seshat_url is not present in device. properties:%s\n", seshat_url);
 
         }
 
@@ -287,12 +314,12 @@ static int addParodusCmdToFile(char *command)
 	fp = fopen("/tmp/parodusCmd.cmd", "w");
 	if (fp == NULL)
 	{
-		LogInfo("Cannot open %s in write mode\n", "/tmp/parodusCmd.cmd");
+		LogError("Cannot open %s in write mode\n", "/tmp/parodusCmd.cmd");
 		return -1;
 	}
 	if (ferror(fp))
 	{
-		LogInfo("Error while writing parodusCmd.cmd file.\n");
+		LogError("Error while writing parodusCmd.cmd file.\n");
 		fclose(fp);
 		return -1;
 	}
@@ -302,8 +329,9 @@ static int addParodusCmdToFile(char *command)
 	return 0;
 }
  
-static void _START_LOG(const char *msg, ...)
+static void _START_LOG(int level, const char *msg, ...)
 {
+	static const char *_level[] = { "Error", "Info" };
 	va_list arg_ptr;
 	int nbytes;
 	char buf[MAX_BUF_SIZE];
@@ -326,5 +354,294 @@ static void _START_LOG(const char *msg, ...)
 	{
 	    buf[nbytes] = '\0';
 	} 	
-	printf("%s : [%s] %s", curtime, MODULE, buf);
+	printf("%s : [mod=%s, lvl=%s] %s", curtime, MODULE, _level[level], buf);
+}
+
+void getFirmwareFromJson(cJSON **json, char * cfgJson_firmware)
+{
+	char *data = NULL;
+	cJSON *oldFirmwareObj = NULL;
+	FILE *fileRead = NULL;
+	int len;
+	fileRead = fopen( WEBPA_CFG_FILE, "r+" );    
+	if( fileRead == NULL ) 
+	{
+	    LogError( "Error opening file in read mode\n" );
+	}
+	
+	fseek( fileRead, 0, SEEK_END );
+	len = ftell( fileRead );
+	fseek( fileRead, 0, SEEK_SET );
+	data = ( char* )malloc( len + 1 );
+	fread( data, 1, len, fileRead );
+	fclose( fileRead );
+
+	if( data != NULL ) 
+	{
+	    *json = cJSON_Parse( data );
+	    if( !*json ) 
+	    {
+	        LogError( "json parse error: [%s]\n", cJSON_GetErrorPtr() );
+	    } 
+	    else 
+	    {
+	        oldFirmwareObj = cJSON_GetObjectItem( *json, WEBPA_CFG_FIRMWARE_VER );
+	        if( oldFirmwareObj != NULL) 
+	        {
+	            char *oldFirmware = cJSON_GetObjectItem( *json, WEBPA_CFG_FIRMWARE_VER )->valuestring;
+	            strcpy(cfgJson_firmware, oldFirmware);
+        	}
+       
+	    }
+	    free(data);
+	    data = NULL;
+	}
+
+}
+
+
+static int writeToJson(char *data)
+{
+    FILE *fp;
+    fp = fopen(WEBPA_CFG_FILE, "w");
+    if (fp == NULL) 
+    {
+        LogError("Failed to open file %s\n", WEBPA_CFG_FILE);
+        return -1;
+    }
+    
+    fwrite(data, strlen(data), 1, fp);
+    fclose(fp);
+    return 0;
+}
+
+static void getValuesFromPsmDb(char *names[], char **values,int count)
+{
+    FILE* out = NULL;
+    char command[MAX_BUF_SIZE]={'\0'};
+    char buf[MAX_BUF_SIZE] = {'\0'};
+    char tempBuf[MAX_BUF_SIZE] ={'\0'};
+    int offset = 0, i=0, index=0;
+    char temp[MAX_VALUE_SIZE] = {'\0'};
+
+    for(i=0; i<count; i++)
+    {
+        offset += snprintf(tempBuf + offset, sizeof(tempBuf) - offset, " %dX %s%s", i, pathPrefix, names[i]);
+    }
+
+    snprintf(command, sizeof(command),"psmcli get -e%s", tempBuf);
+    LogInfo("command : %s\n",command);
+
+    out = popen(command, "r");
+    if(out)
+    {
+        for(i=0; i<count; i++)
+        {
+            fgets(buf, sizeof(buf), out);
+	    if(strlen(buf) > 0)
+	    {
+                *strrchr(buf, '"') = '\0';
+                values[i] = (char *) malloc(sizeof(char)* MAX_VALUE_SIZE);
+                sscanf(buf, "%dX=\"%s\n", &index, temp);
+		if(index == i)
+		{
+		    strcpy(values[i], temp);
+		}
+	        else
+	        {
+		    free(values[i]);
+		    values[i] = NULL;
+	        }
+	    }
+        }
+        pclose(out);
+    }
+    else
+    {
+        LogError("Failed to execute command\n");
+    }
+}
+
+static int setValuesToPsmDb(char *names[], char **values,int count)
+{
+    FILE* out = NULL;
+    char command[MAX_BUF_SIZE]={'\0'};
+    char buf[MAX_BUF_SIZE] = {0};
+    int i = 0, ret=0;
+    char tempBuf[MAX_BUF_SIZE] ={0};
+    int offset = 0;
+
+    for(i=0; i<count; i++)
+    {
+        offset += snprintf(tempBuf + offset, sizeof(tempBuf) - offset, " %s%s %s", pathPrefix,names[i], values[i]);
+    }
+
+    snprintf(command, sizeof(command),"psmcli set%s", tempBuf);
+    LogInfo("command : %s\n",command);
+    out = popen(command, "r");
+    if(out)
+    {
+        for(i=0; i<count; i++)
+        {
+            fgets(buf, sizeof(buf), out);
+            sscanf(buf, "%d\n", &ret);
+            if(ret != 100)
+            {
+                LogError("Failed to setValuesToPsmDb\n");
+                pclose(out);
+                return -1;
+            }
+        }
+        pclose(out);
+    }
+    else
+    {
+        LogError("Failed to execute command\n");
+        return -1;
+    }
+    return 0;
+}
+
+static void getValuesFromSysCfgDb(char *names[], char **values,int count)
+{
+    int i = 0;
+    for(i=0; i<count; i++)
+    {
+    	char temp[MAX_VALUE_SIZE] ={'\0'};
+        if(syscfg_get( NULL, names[i], temp, MAX_VALUE_SIZE) == 0)
+        {
+	    	values[i] = (char *) malloc(sizeof(char)* MAX_VALUE_SIZE);
+	    	strcpy(values[i], temp);
+        }
+    }
+}
+
+static int syncXpcParamsOnUpgrade(char *lastRebootReason, char *firmwareVersion)
+{
+	int paramCount = 0, status = 0, i = 0;
+	cJSON *json = NULL;
+	char cfgJson_firmware[64]={'\0'};
+	int configUpdateStatus = -1;
+        char *out =NULL;
+        cJSON *FirmwareObj = NULL;
+        char *paramList[] = {"X_COMCAST-COM_CMC","X_COMCAST-COM_CID","X_COMCAST-COM_SyncProtocolVersion"};
+	char *psmValues[MAX_VALUE_SIZE] = {'\0'};
+	char *sysCfgValues[MAX_VALUE_SIZE] = {'\0'};
+
+	paramCount = sizeof(paramList)/sizeof(paramList[0]);
+	
+	getFirmwareFromJson(&json, cfgJson_firmware);
+	LogInfo("cfgJson_firmware fetched from webpa_cfg.json is %s\n", cfgJson_firmware);
+	
+	getValuesFromPsmDb(paramList, psmValues, paramCount);
+	for(i = 0; i<paramCount; i++)
+	{
+	        if(psmValues[i] == NULL)
+	        {
+	        	LogInfo("PsmDb-> value is NULL for %s\n",paramList[i]);
+	        	free_sync_db_items(paramCount, psmValues, sysCfgValues, json);
+	        	return -1;
+	        }
+	        else
+	        {
+	        	LogInfo("PsmDb-> %s value is %s\n",paramList[i], psmValues[i]);
+	        }
+	}
+	
+	/* To check if it is an upgrade from release image to parodus ON */
+	
+	if((strcmp(lastRebootReason,"Software_upgrade")==0 || ((strlen(cfgJson_firmware)>0) && (strcmp(firmwareVersion, cfgJson_firmware))!=0)) && (atoi(psmValues[0]) ==0 && strcmp(psmValues[1], "0")==0 && strcmp(psmValues[2] ,"0")==0))
+	{
+		LogInfo("sync for bbhm and syscfg is required. Proceeding with DB sync..\n");
+		getValuesFromSysCfgDb(paramList, sysCfgValues, paramCount);
+		for(i=0; i<paramCount; i++)
+		{
+			if(sysCfgValues[i] == NULL)
+	        	{
+	        		LogInfo("SysCfgDb-> value is NULL for %s\n", paramList[i]);
+	        		free_sync_db_items(paramCount, psmValues, sysCfgValues, json);
+	        		return -2;
+	        	}
+	        	else
+	        	{
+	        		LogInfo("SysCfgDb-> %s value is %s\n", paramList[i], sysCfgValues[i]);
+	        	}
+		}
+		
+		status = setValuesToPsmDb(paramList, sysCfgValues, paramCount);
+		if(status == 0)
+		{
+		        LogInfo("Successfully set values to PSM DB\n");
+			if(json != NULL)
+			{
+				FirmwareObj = cJSON_GetObjectItem( json, WEBPA_CFG_FIRMWARE_VER );
+				if (FirmwareObj != NULL)
+				{
+					cJSON_ReplaceItemInObject(json, WEBPA_CFG_FIRMWARE_VER, cJSON_CreateString(firmwareVersion));
+					out = cJSON_Print(json);
+					LogInfo("Updated json content is %s\n", out);
+					configUpdateStatus = writeToJson(out);
+		
+					if(configUpdateStatus == 0)
+			    		{
+			    			LogInfo("After db sync, updated Firmware version to config file\n");
+			    		}
+			    		else
+			    		{
+			    			LogError("Error in updating Firmware version to config file\n");
+			    		}
+			    		
+			    		if(out !=NULL)
+			    		{
+			    			free(out);
+			    			out = NULL;
+			    		}
+		    		}
+		    		else
+		    		{
+		    			LogError("oldFirmwareVersion is not present in config\n");
+		    		}
+	    		}
+    		}
+    		else
+    		{
+    			LogError("Failed to set values to PSM DB\n");
+    			free_sync_db_items(paramCount, psmValues, sysCfgValues, json);
+    			return -2;
+    		}
+    		
+	}
+	else
+	{
+		LogInfo("Sync for bbhm and syscfg is not required\n");
+		free_sync_db_items(paramCount, psmValues, sysCfgValues, json);
+		return -1;
+	}
+	
+	free_sync_db_items(paramCount, psmValues, sysCfgValues, json);
+	return 0;
+}
+
+static void free_sync_db_items(int paramCount, char *psmValues[], char *sysCfgValues[], cJSON *json)
+{
+	int i;
+	for(i = 0; i<paramCount; i++)
+	{
+		if(psmValues[i] != NULL)
+		{
+	       		free(psmValues[i]);
+	       		psmValues[i] = NULL;
+	       	}
+	       	
+	       	if(sysCfgValues[i] != NULL)
+		{
+	       		free(sysCfgValues[i]);
+	       		sysCfgValues[i] = NULL;
+	       	}
+	}
+	if(json != NULL)
+	{
+		cJSON_Delete(json);
+	}
+
 }
